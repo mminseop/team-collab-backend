14e94e72fe22817fd606b48c0b9850f906e11799
mminseop <alstjq903@naver.com>
2025-11-29 22:39:27 +0900

    Fix: db.ts db명 수정

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,9 +3,9 @@ import mysql from 'mysql2/promise';
 const db = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT || '3306'),
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'teamcollab',
+  user: process.env.DB_USER || 'dbuser',  
+  password: process.env.DB_PASSWORD || 'alstjq9965',
+  database: process.env.DB_NAME || 'team_collab',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
